1af8ad18deaab902745c7ccc9bac4418f79e4cb3
Thomas Torres <thomas.matt.torres@gmail.com>
2013-07-22 16:36:33 -0500

    Couple of fixes.

--- crisix/reset.py
+++ crisix/reset.py
@@ -3,5 +3,5 @@ import settings
 
 os.system('echo "yes" | python manage.py reset database')
 os.system('manage.py syncdb')
-for f in glob.glob(settings.STATIC_ROOT + '/thumbs/*'):
+for f in glob.glob(settings.THUMB_ROOT + '/*'):
     os.remove(f)
