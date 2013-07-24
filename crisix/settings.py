5b87f5834414bc1743d02457384c06ee51dccff6
tmtorres <thomas.matt.torres@gmail.com>
2013-07-24 15:45:11 -0500

    db again

--- crisix/settings.py
+++ crisix/settings.py
@@ -1,6 +1,7 @@
 # Django settings for crisix project.
 import os, sys
 from os.path import dirname
+import dj_database_url
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -14,18 +15,8 @@ LOCKDOWN_PASSWORDS = ('django')
 MANAGERS = ADMINS
 
 DATABASES = {
-    'default': {
-        'ENGINE': '', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
-
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
