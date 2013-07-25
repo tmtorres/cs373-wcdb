197c7700511a14e96a7207f3a9a7d18b341a31b7
Jonathan Velasquez <jonathan@cs.utexas.edu>
2013-07-25 18:24:30 -0500

    documentation for env.py

--- env.py
+++ env.py
@@ -1,2 +1,7 @@
+"""
+env.py is a script used to set the environment for local development
+in order to access restricted binary and source files in order to
+activate a proper virtual environment
+"""
 import os
 os.system('/bin/bash --rcfile /u/tmtorres/CS373/cs373-wcdb/activate.sh')
