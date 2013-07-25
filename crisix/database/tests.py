7c2c00204bc6afe8b986f0ffcdf67bf4b754b3d2
tmtorres <thomas.matt.torres@gmail.com>
2013-07-24 22:20:51 -0500

    Tester.

--- crisix/database/tests.py
+++ crisix/database/tests.py
@@ -166,7 +166,7 @@ class SimpleTest(TestCase, RequestFactory):
 
 class TestUpload(TestCase):
     def test_insert_1(self):
-        root = fromstring(open('TestCrisis.xml').read())
+        root = fromstring(open(os.path.join(settings.BASE_DIR, 'crisix/TestCrisis.xml')).read())
         insert(root)
         c = None
 
@@ -314,59 +314,218 @@ class TestUpload(TestCase):
         self.assertEqual(str(p.location), 'Communist Party of China')
 
     def test_insert_elem_1(self):
-        pass
+        root = fromstring(open('TestPerson.xml').read())
+        perHandler(root[0])
+        p = Person.objects.get(id='PER_BROBMA')
+
+        # Test is proper amount of Videos are in DB
+        element = p.elements.filter(ctype='VID')
+        self.assertEqual(len(element), 2)
+        # Test is insertion works without prior functions
+        insertElem({'href':'www.youtube.com/videos'}, {'entity':p, 'ctype':'VID', 'text': 'Test Youtube Video'})
+        element = str(p.elements.filter(ctype='VID'))
+        self.assertNotEqual(element.find('www.youtube.com/videos'), -1)
+        element = p.elements.filter(ctype='VID')
+        self.assertEqual(len(element), 3)
+        element = p.elements.filter(ctype='CITE')
+        self.assertEqual(len(element), 1)
         
     def test_insert_elem_2(self):
-        pass
+        root = fromstring(open('TestCrisis.xml').read())
+        criHandler(root[0])
+        c = Crisis.objects.get(id='CRI_SHESSG')
+
+        # Test is proper amount of Videos are in DB
+        element = c.elements.filter(ctype='VID')
+        self.assertEqual(len(element), 1)
+        # Test is insertion works without prior functions
+        insertElem({'href':'www.youtube.com/videos'}, {'entity':c, 'ctype':'VID', 'text': 'Test Youtube Video'})
+        element = str(c.elements.filter(ctype='VID'))
+        self.assertNotEqual(element.find('www.youtube.com/videos'), -1)
+        element = c.elements.filter(ctype='VID')
+        self.assertEqual(len(element), 2)
+        element = c.elements.filter(ctype='CITE')
+        self.assertEqual(len(element), 3)
 
     def test_insert_elem_3(self):
-        pass
+        root = fromstring(open('TestOrganization.xml').read())
+        orgHandler(root[0])
+        o = Organization.objects.get(id='ORG_UNDWAY')
+
+        # Test is proper amount of Videos are in DB
+        element = o.elements.filter(ctype='VID')
+        self.assertEqual(len(element), 0)
+        # Test is insertion works without prior functions
+        insertElem({'href':'www.youtube.com/videos'}, {'entity':o, 'ctype':'VID', 'text': 'Test Youtube Video'})
+        element = str(o.elements.filter(ctype='VID'))
+        self.assertNotEqual(element.find('www.youtube.com/videos'), -1)
+        element = o.elements.filter(ctype='VID')
+        self.assertEqual(len(element), 1)
+        element = o.elements.filter(ctype='MAP')
+        self.assertEqual(len(element), 0)
 
     def test_com_handler_1(self):
-    	pass
+    	root = fromstring(open('TestPerson.xml').read())
+        perHandler(root[0])
+        p = Person.objects.get(id='PER_BROBMA')
+
+        comHandler(root[0], p)
+        p.save()
+        summary = p.summary
+        self.assertNotEqual(summary.find('Barack Obama is the 44th President of the United States'), -1)
 
     def test_com_handler_2(self):
-        pass
+        root = fromstring(open('TestCrisis.xml').read())
+        criHandler(root[0])
+        c = Crisis.objects.get(id='CRI_SHESSG')
+
+        comHandler(root[0], c)
+        c.save()
+        summary = c.summary
+        self.assertNotEqual(summary.find('Adam Lanza is believed to have shot his mother'), -1)
 
     def test_com_handler_3(self):
-        pass
+        root = fromstring(open('TestOrganization.xml').read())
+        orgHandler(root[0])
+        o = Organization.objects.get(id='ORG_UNDWAY')
+
+        comHandler(root[0], o)
+        o.save()
+        summary = o.summary
+        self.assertNotEqual(summary.find('The Sandy Hook School SupportFund (SHSSF) was created by United Way of Western Connecticut'), -1)
 
 class TestDownload(TestCase):
     def test_get_crises_1(self):
-    	pass
-        
+    	test = fromstring(open('TestCrisis.xml').read())
+    	insert(test)
+    	
+    	root = ET.Element('WorldCrises')
+    	getCrises(root)
+    	
+    	self.assertEqual(root[0][2].text, 'Spree Shooting')
+    	self.assertEqual(root[0][3].text, '2012-12-14')
+    	self.assertEqual(root[0][4].text, '09:35:00')
+       
     def test_get_crises_2(self):
-        pass
+    	test = fromstring(open('TestCrisis.xml').read())
+    	insert(test)
+    	
+    	root = ET.Element('WorldCrises')
+    	getCrises(root)
+    	
+    	self.assertEqual(root[1][2].text, 'Earthquake')
+    	self.assertEqual(root[1][3].text, '2008-05-12')
+    	self.assertEqual(root[1][4].text, '14:28:01')
 
     def test_get_crises_3(self):
-        pass
+    	test = fromstring(open('TestCrisis.xml').read())
+    	insert(test)
+    	
+    	root = ET.Element('WorldCrises')
+    	getCrises(root)
+    	
+    	self.assertEqual(root[2][2].text, 'Civil War')
+    	self.assertEqual(root[2][3].text, '2011-02-15')
 
     def test_get_organizations_1(self):
-        pass
+    	test = fromstring(open('TestOrganization.xml').read())
+    	insert(test)
+    	
+    	root = ET.Element('WorldCrises')
+    	getOrganizations(root)
+    	
+    	self.assertEqual(root[0][2].text, 'Non-profit Organization')
+    	self.assertEqual(root[0][3].text, 'Worldwide')
 
     def test_get_organizations_2(self):
-        pass
+    	test = fromstring(open('TestOrganization.xml').read())
+    	insert(test)
+    	
+    	root = ET.Element('WorldCrises')
+    	getOrganizations(root)
+    	
+    	self.assertEqual(root[1][2].text, 'Non-profit, humanitarian Organization')
+    	self.assertEqual(root[1][3].text, 'Chengdu, China')
 
     def test_get_organizations_3(self):
-        pass
+    	test = fromstring(open('TestOrganization.xml').read())
+    	insert(test)
+    	
+    	root = ET.Element('WorldCrises')
+    	getOrganizations(root)
+    	
+    	self.assertEqual(root[2][2].text, 'de facto Government')
+    	self.assertEqual(root[2][3].text, 'Benghazi, Libya')
 
     def test_get_people_1(self):
-        pass
+    	test = fromstring(open('TestPerson.xml').read())
+    	insert(test)
+    	
+    	root = ET.Element('WorldCrises')
+    	getPeople(root)
+    	
+    	self.assertEqual(root[0][2].text, 'Murderer')
+    	self.assertEqual(root[0][3].text, 'Newtown, Connecticut')
 
     def test_get_people_2(self):
-        pass
+    	test = fromstring(open('TestPerson.xml').read())
+    	insert(test)
+    	
+    	root = ET.Element('WorldCrises')
+    	getPeople(root)
+    	
+    	self.assertEqual(root[1][2].text, 'President')
+    	self.assertEqual(root[1][3].text, 'Washington, D.C, United States of America')
 
     def test_get_people_3(self):
-        pass
+    	test = fromstring(open('TestPerson.xml').read())
+    	insert(test)
+    	
+    	root = ET.Element('WorldCrises')
+    	getPeople(root)
+    	
+    	self.assertEqual(root[2][2].text, 'President')
+    	self.assertEqual(root[2][3].text, 'Communist Party of China')
 
     def test_get_common_1(self):
-        pass
+    	test = fromstring(open('TestCrisis.xml').read())
+    	insert(test)
+    	
+    	root = ET.Element('WorldCrises')
+        c = Crisis.objects.get(id='CRI_SHESSG')   	
+    	x = ET.SubElement(root, 'Crisis')
+        getCommon(x, c)
+        
+        c.save()
+        summary= c.summary
+        self.assertNotEqual(summary.find('According to reports, most of the shooting'), -1)
+
 
     def test_get_common_2(self):
-        pass
+        test = fromstring(open('TestOrganization.xml').read())
+    	insert(test)
+    	
+    	root = ET.Element('WorldCrises')
+        c = Organization.objects.get(id='ORG_UNDWAY')   	
+    	x = ET.SubElement(root, 'Organization')
+        getCommon(x, c)
+        
+        c.save()
+        summary= c.summary
+        self.assertNotEqual(summary.find('Immediately following the tragedy on December 14, 2012,'), -1)
 
     def test_get_common_3(self):
-        pass
+        test = fromstring(open('TestPerson.xml').read())
+    	insert(test)
+    	
+    	root = ET.Element('WorldCrises')
+        c = Person.objects.get(id='PER_BROBMA')   	
+    	x = ET.SubElement(root, 'Person')
+        getCommon(x, c)
+        
+        c.save()
+        summary= c.summary
+        self.assertNotEqual(summary.find('He was re-elected president in November 2012,'), -1)
 
 class TestViews(TestCase):
     def test_basic_addition(self):
