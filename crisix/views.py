0fe66d446a6495c7b820e74039ef2109a077b0ee
Thomas Torres <thomas.matt.torres@gmail.com>
2013-08-03 20:45:40 -0500

    Migrated organizations to new template

--- crisix/views.py
+++ crisix/views.py
@@ -129,11 +129,12 @@ def organizations(request, id):
     o = Organization.objects.get(id='ORG_' + str(id).upper())
     return render(request, 'organization.html', {
         'o' : o,
+        'summary': paragraph_split(o.summary),
         'related_crises' : [{'id': str(c.id).lower()[4:], 'name': c.name} for c in o.crises.all()],
         'related_people' : [{'id': str(p.id).lower()[4:], 'name': p.name} for p in o.people.all()],
         'citations' : [{'href': w.href, 'text': w.text} for w in o.elements.filter(ctype='CITE')],
         'contact' : [{'href': li.attrib.get('href'), 'text': li.text} for li in fromstring('<ContactInfo>' + o.contact + '</ContactInfo>')],
-        'history': ''.join(o.history.split('<li>')).replace('</li>', ' ').strip(),
+        'history': paragraph_split(convert_li(o.history)),
         'feeds' : [{'id': str(w.embed).split('/')[-1]} for w in o.elements.filter(ctype='FEED')],
         'maps' : [{'embed': w.embed, 'text': w.text} for w in o.elements.filter(ctype='MAP')],
         'images' : generate_thumbs(o),
